f212f14704be359c3a74b8c9362328aa0835dd5f
monokoo <monokoo@users.noreply.github.com>
2020-11-02 21:58:32 +0800

    更新luci-app-vlmcsd

--- package/lean/luci-app-vlmcsd/luasrc/controller/vlmcsd.lua
+++ package/lean/luci-app-vlmcsd/luasrc/controller/vlmcsd.lua
@@ -1,19 +1,20 @@
+--[[
+openwrt-dist-luci: ShadowSocks
+]]--
+
 module("luci.controller.vlmcsd", package.seeall)
 
 function index()
 	if not nixio.fs.access("/etc/config/vlmcsd") then
 		return
 	end
-	local page
-	page = entry({"admin", "services", "vlmcsd"}, cbi("vlmcsd"), _("KMS Server"), 100)
-	page.i18n = "vlmcsd"
-	page.dependent = true
+	
+	entry({"admin", "services", "vlmcsd"}, cbi("vlmcsd"), _("KMS服务器")).dependent = true
 	entry({"admin","services","vlmcsd","status"},call("act_status")).leaf=true
 end
-
 function act_status()
-  local e={}
-  e.running=luci.sys.call("pgrep vlmcsd >/dev/null")==0
-  luci.http.prepare_content("application/json")
-  luci.http.write_json(e)
+local e={}
+e.vlmcsd=luci.sys.call("pgrep /usr/bin/vlmcsd >/dev/null")==0
+luci.http.prepare_content("application/json")
+luci.http.write_json(e)
 end
--- package/lean/luci-app-vlmcsd/luasrc/model/cbi/vlmcsd.lua
+++ package/lean/luci-app-vlmcsd/luasrc/model/cbi/vlmcsd.lua
@@ -1,35 +1,61 @@
-
+local NXFS = require "nixio.fs"
+local SYS  = require "luci.sys"
 
 m = Map("vlmcsd")
-m.title	= translate("vlmcsd config")
-m.description = translate("A KMS Server Emulator to active your Windows or Office")
-
-m:section(SimpleSection).template  = "vlmcsd/vlmcsd_status"
+m.title	= translate("KMS服务器")
+m:section(SimpleSection).template="vlmcsd/vlmcsd_status"
 
 s = m:section(TypedSection, "vlmcsd")
-s.addremove = false
-s.anonymous = true
+s.anonymous=true
 
-s:tab("basic", translate("Basic Setting"))
-enable = s:taboption("basic",Flag, "enabled", translate("Enable"))
-enable.rmempty = false
+--基本设置
+s:tab("basic", translate("基本设置"))
 
-autoactivate = s:taboption("basic", Flag, "autoactivate", translate("Auto activate"))
-autoactivate.rmempty = false
+o = s:taboption("basic", Flag, "enable")
+o.title = translate("开启")
+o.rmempty = false
 
-s:tab("config", translate("Config File"))
-config = s:taboption("config", Value, "config", translate("configfile"), translate("This file is /etc/vlmcsd.ini."), "")
-config.template = "cbi/tvalue"
-config.rows = 13
-config.wrap = "off"
+o = s:taboption("basic", Flag, "use_conf_file")
+o.title = translate("使用配置自动激活")
+o.rmempty = false
 
-function config.cfgvalue(self, section)
-	return nixio.fs.readfile("/etc/vlmcsd.ini")
+o = s:taboption("basic", Value, "port")
+o.title = translate("本地端口")
+o.datatype = "port"
+o.default = 1688
+o.placeholder = 1688
+o:depends("use_conf_file",0)
+
+o = s:taboption("basic", Flag, "enable_web")
+o.title = translate("开启外网访问")
+o.default = 0
+o.rmempty = false
+
+--配置
+s:tab("config", translate("配置"))
+
+local file = "/usr/share/vlmcsd/vlmcsd.ini"
+o = s:taboption("config", TextValue, "configfile")
+o.description = translate("开头数字符号（＃）或分号（;）的每一行被视为注释，去除分号（;）启用选项。")
+o.rows = 20
+o.wrap = "off"
+o.cfgvalue = function(self, section)
+	return NXFS.readfile(file) or ""
+end
+o.write = function(self, section, value)
+	NXFS.writefile(file, value:gsub("\r\n", "\n"))
 end
 
-function config.write(self, section, value)
-	value = value:gsub("\r\n?", "\n")
-	nixio.fs.writefile("/etc/vlmcsd.ini", value)
+s:tab("log", translate("日志记录"))
+
+local file = "/var/log/vlmcsd.log"
+o = s:taboption("log", TextValue, "logfile")
+o.rows = 20
+o.wrap = "on"
+o.cfgvalue = function(self, section)
+	return NXFS.readfile(file) or ""
+end
+o.write = function(self, section, value)
 end
 
 return m
