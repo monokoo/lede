122b5ca529c1367aadec1cb878d1a0198ade84cd
monokoo <monokoo@users.noreply.github.com>
2020-11-05 23:52:45 +0800

    兼容master分支新版本luci

--- package/lean/luci-app-ramfree/luasrc/controller/release_ram.lua
+++ package/lean/luci-app-ramfree/luasrc/controller/release_ram.lua
@@ -1,9 +1,9 @@
 module("luci.controller.release_ram",package.seeall)
 
 function index()
-	entry({"admin","status","release_ram"}, call("release_ram"), _("Release Ram"), 9999)
+	entry({"admin","system","release_ram"}, call("release_ram"), _("Release Ram"), 9999)
 end
 function release_ram()
-	luci.sys.call("sync && echo 3 > /proc/sys/vm/drop_caches")
-	luci.http.redirect(luci.dispatcher.build_url("admin/status"))
+	luci.util.exec("sync && echo 3 > /proc/sys/vm/drop_caches")
+	luci.http.redirect(luci.dispatcher.build_url("admin","status","overview"))
 end
